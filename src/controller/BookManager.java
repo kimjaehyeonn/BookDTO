bfb5879cf49845099b10930518f98fb3c24161a0
jaehyeonngit <jhjhh023@gmail.com>
2025-05-05 22:18:24 +0900

    Feat: BookManager 클래스 메서드 구현

--- src/controller/BookManager.java
+++ src/controller/BookManager.java
@@ -9,29 +9,41 @@ public class BookManager {
 
     // 도서 추가
     public void addBook(BookDTO book) {
-
+        booklist.add(book);
     }
 
     // 전달 받은 index로 도서 삭제
     public void deleteBook(int index){
+
+        booklist.remove(index);
 
     }
 
     // 도서명이 일치하는 객체 찾아 해당 index 리턴
     // 도서명이 일치하는 객체가 리스트에 없으면 -1 리턴
     public int searchBook(String title) {
+        return booklist.indexOf(title);
 
     }
 
     // 인덱스 위치의 객체 정보 출력
     // 인덱스 도서가 없으면 적절한 메시지 출력
     public void printBook (int index) {
-
+        if (false) {
+            System.out.println("원하시는 도서가 존재하지 않습니다.");
+        } else {
+            System.out.println(booklist.get(index));
+        }
     }
 
     // 도서 전체 리스트 출력
     // 도서가 없는 경우 적절한 메시지 출력
-    public void displayAll() {}
+    public void displayAll() {
+        for (BookDTO book : booklist) {
+            System.out.println(book.getBNo() + "\t" + book.getCategory() + "\t" + book.getTitle() + "\t"+ book.getAuthor());
+
+        };
+    }
 
 
 }
